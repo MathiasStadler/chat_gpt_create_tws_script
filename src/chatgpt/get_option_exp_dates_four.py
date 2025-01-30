ba5f6cadbf4287b22b841356ca4004a93c66a7a4
Mathias Stadler <stadler-mathias@web.de>
2025-01-30 17:37:26 +0100

    save option chain

---
+++ src/chatgpt/get_option_exp_dates_four.py
@@ -0,0 +1,81 @@
+# Python script to connect to TWS and retrieve stock and option data
+# Python version: 3.x (latest stable version)
+
+# Install necessary packages
+# pip install ib-insync
+
+from ib_insync import *
+from ibapi.common import MarketDataTypeEnum
+
+
+
+
+# Connect to TWS
+ib = IB()
+ib.connect('127.0.0.1', 7496, clientId=1)
+
+# Set market data type to delayed
+ib.reqMarketDataType(MarketDataTypeEnum.DELAYED)
+
+# Define the stock contract for TREX
+# stock_contract = Stock('TREX', 'SMART', 'USD')
+
+
+
+# Qualify the contract
+qualified_contracts = ib.qualifyContracts(stock_contract)
+
+
+if not qualified_contracts:
+    print("No qualified contracts found.")
+else:
+    stock_contract = qualified_contracts[0]
+
+    # Request market data
+    ib.reqMktData(stock_contract, '', False, False)
+
+# FROM HERE
+# https://github.com/erdewit/ib_insync/blob/master/notebooks/option_chain.ipynb
+#    ticker = ib.reqStock("TREX")
+#    trexValue = ticker
+#    print(trexValue)
+
+    # Print the price of the stock
+    print(f"Price of {stock_contract.symbol}: {stock_contract.strike}")
+
+    # Print the contract ID
+    print(f"Contract ID: {stock_contract.conId}")
+
+    # Retrieve option chain
+    option_chain = ib.reqSecDefOptParams(stock_contract.symbol, '', stock_contract.secType, stock_contract.conId)
+
+    # chains = ib.reqSecDefOptParams(spx.symbol, '', stock_contract.secType, stock_contract.conId)
+    
+   # print(option_chain)
+
+    # util.df(option_chain)
+
+    # Print expiration dates and option chains
+    for option in option_chain:
+        # print(f"Expiration Date: {option.expirations}")
+        
+        # HIER NEXT STEP
+        # https://www.pyquantnews.com/the-pyquant-newsletter/download-options-chains-data-ibkr-api
+        # print(f"strikes => {option.strikes}")
+
+
+        for expiration in option.expirations:
+            # print(expiration)
+            # Request options for each expiration date
+            # options = ib.reqContractDetails(Option(stock_contract.symbol, expiration, 'C', stock_contract.exchange))
+            # print(options)
+
+            # for option in options:
+            #     print(option)
+            #     print("--SPACER--")
+        #    print(f"Call Options for {expiration}: {[opt.contract.conId for opt in options]}")
+            options = ib.reqContractDetails(Option(stock_contract.symbol, expiration, 'P', stock_contract.exchange))
+            # print(f"Put Options for {expiration}: {[opt.contract.conId for opt in options]}")
+
+# Disconnect from TWS
+ib.disconnect()
---
+++ src/human/get_option.py
@@ -0,0 +1,44 @@
+"""
+[FROM HERE](https://stackoverflow.com/questions/56896031/python-ibapi-reqcontractdetails-wont-return-result-when-run-second-time)
+"""
+
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+from ibapi.common import *
+from ibapi.contract import *
+from ContractSamples import ContractSamples
+
+
+class TestApp(EClient, EWrapper):
+    def __init__(self):
+        EClient.__init__(self, self)
+
+    def nextValidId(self, orderId:int):
+        print("id", orderId)
+        contract = Contract()
+        contract.symbol = "IBKR"
+        contract.secType = "STK"
+        contract.exchange = "SMART"
+        contract.currency = "USD"
+        contract.primaryExchange = "NASDAQ"
+
+        self.reqContractDetails(10, contract)
+
+    def error(self, reqId:TickerId, errorCode:int, errorString:str):
+        print("Error: ", reqId, "", errorCode, "", errorString)
+
+    def contractDetails(self, reqId:int, contractDetails:ContractDetails):
+        print("contractDetail: ", reqId, " ", contractDetails)
+
+    def contractDetailsEnd(self, reqId:int):
+        print("end, disconnecting")
+        self.disconnect()
+
+def main():
+    app = TestApp()
+
+    app.connect("127.0.0.1", 7496, 33)
+    app.run()
+
+if __name__ == "__main__":
+    main()
