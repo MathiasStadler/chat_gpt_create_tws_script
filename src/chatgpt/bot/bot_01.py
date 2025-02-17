b1c4272d812e84bc406d83dfb9473dcacb871a63
Mathias Stadler <stadler-mathias@web.de>
2025-02-17 19:40:18 +0100

    save

---
+++ src/chatgpt/bot/bot_01.py
@@ -0,0 +1,8 @@
+1. OOP based
+2. with try and error handling for each method
+3. with main function
+4. without  any test case
+5. use the latest stable Python Library
+7. use the Python Yahoo Finance API 
+8. Create a list on start of the script with command how to install all necessary library
+9. the pramm should restart netx weekday for start the stock, create therefor a linux CRON job
---
+++ src/deepseek/probability_cones/p1.py
@@ -0,0 +1,18 @@
+"""
+    1. OOP based
+    2. with try and error handling for each method
+    3. with main function
+    4. without  any test case
+    5. use the latest stable Python Library
+    6. use the Yahoo Finance API 
+    7. use the Python Library Plotly Express
+    8. Put as command how to install all necessary library
+    9. adapt the y axis on the value, leave open 10% and under 10% space
+    10. save download data inside a csv file with name of stock
+    11. don't overwrite any data files. Check first whether the file exists and if it exists, create a new one
+    12. get the data for the last 24 Month
+    13. print a chart last  12 month of the stock from first argument.  Use for this the library plotly
+    14  add the Simple Moving average  200 in red, Simple Moving average  60 in yellow and the Simple Moving average green
+    15. add the indicator Bollinger Bands and make the space between the bands in high blue
+    16. gathering the stock ticker from first argument of script call
+"""
